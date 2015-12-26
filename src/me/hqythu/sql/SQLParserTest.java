08c9abe11aa32ee94d245bde5c15861feb93b3cd
lxh <86849081@qq.com>
2015-12-26 16:18:31 +0800

    finish most base function

--- src/me/hqythu/sql/SQLParserTest.java
+++ src/me/hqythu/sql/SQLParserTest.java
@@ -1,5 +1,6 @@
 package me.hqythu.sql;
 
+import me.hqythu.manager.SystemManager;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,11 +10,24 @@ import static org.junit.Assert.*;
  * Created by apple on 15/12/24.
  */
 public class SQLParserTest {
+
+    public static final String TEST_DB = "hello";
+
     @Test
     public void testCreateDataBase() {
-        ParseResult sql = SQLParser.parse("CREATE DATABASE hello;");
-        Assert.assertTrue("创建数据库",sql.type == ParseResult.OrderType.CREATE_DATABASE);
-        Assert.assertTrue(sql.dataBaseName.equals("hello"));
+        ParseResult sql;
+
+        // create DataBase
+        sql = SQLParser.parse("CREATE DATABASE "+TEST_DB+";");
+        Assert.assertTrue(sql.type == ParseResult.OrderType.CREATE_DATABASE);
+        Assert.assertTrue(sql.dataBaseName.equals(TEST_DB));
+        Assert.assertTrue(SystemManager.getInstance().createDatabase(sql.dataBaseName));
+
+        // drop DataBase
+        sql = SQLParser.parse("DROP DATABASE "+TEST_DB+";");
+        Assert.assertTrue(sql.type == ParseResult.OrderType.DROP_DATABASE);
+        Assert.assertTrue(sql.dataBaseName.equals(TEST_DB));
+        Assert.assertTrue(SystemManager.getInstance().dropDatabase(sql.dataBaseName));
     }
 
     @Test
