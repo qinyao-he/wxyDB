631725b36c07573fa50f99209cadaf612acbc1ce
lxh <86849081@qq.com>
2015-12-27 21:06:14 +0800

    add SelectOption support *

--- src/me/hqythu/wxydb/util/SelectOption.java
+++ src/me/hqythu/wxydb/util/SelectOption.java
@@ -7,15 +7,29 @@ import java.util.Set;
 
 public class SelectOption {
     public SelectOption() {
+        this.all = false;
         tableNames = new ArrayList<>();
         columnNames = new ArrayList<>();
         fromTableNames = new HashSet<>();
     }
-
+    public SelectOption(boolean all) {
+        if (all) {
+            this.all = true;
+        } else {
+            this.all = false;
+            tableNames = new ArrayList<>();
+            columnNames = new ArrayList<>();
+            fromTableNames = new HashSet<>();
+        }
+    }
+    public boolean all;
     public List<String> tableNames;
     public List<String> columnNames;
     public Set<String> fromTableNames;
 
+    public void setAll() {all = true;}
+    public void clear() {all = false;}
+    public boolean isAll() {return all;}
     public void add(String tableName, String columnName) {
         tableNames.add(tableName);
         fromTableNames.add(tableName);
