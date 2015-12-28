7296bf6fd81a326a2fc40b9141e92f7d7145e604
何钦尧 <hqythu@gmail.com>
2015-12-29 03:39:27 +0800

    basic ui component for execute tab

--- src/me/hqythu/wxydb/ui/ExeSQLTab.java
+++ src/me/hqythu/wxydb/ui/ExeSQLTab.java
@@ -1,11 +1,18 @@
 package me.hqythu.wxydb.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by hqythu on 12/29/2015.
  */
 public class ExeSQLTab extends JPanel {
+
+    private JTextField sqlTextField;
+    private JButton sqlExeButton;
+    private JTable executeResultTable;
+    private JScrollPane resultScroll;
+    private JPanel controlPanel;
 
     public ExeSQLTab() {
         SwingUtilities.invokeLater(new Runnable() {
@@ -17,7 +24,19 @@ public class ExeSQLTab extends JPanel {
     }
 
     private void createUi() {
+        sqlTextField = new JTextField();
+        sqlTextField.setPreferredSize(new Dimension(400, 24));
+        sqlExeButton = new JButton("Execute");
+        executeResultTable = new JTable();
+        resultScroll = new JScrollPane(executeResultTable);
 
+        controlPanel = new JPanel();
+        controlPanel.add(sqlTextField);
+        controlPanel.add(sqlExeButton);
+
+        setLayout(new BorderLayout());
+        add(controlPanel, BorderLayout.NORTH);
+        add(resultScroll, BorderLayout.CENTER);
     }
 
 }
