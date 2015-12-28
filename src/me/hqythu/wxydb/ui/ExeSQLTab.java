28e09ca5099572dd850c79a0f305f2d09313b472
何钦尧 <hqythu@gmail.com>
2015-12-29 04:06:50 +0800

    ui: table view of exe result

--- src/me/hqythu/wxydb/ui/ExeSQLTab.java
+++ src/me/hqythu/wxydb/ui/ExeSQLTab.java
@@ -1,7 +1,16 @@
 package me.hqythu.wxydb.ui;
 
+import me.hqythu.wxydb.manager.QueryEngine;
+import me.hqythu.wxydb.sql.ParseResult;
+import me.hqythu.wxydb.sql.SQLParser;
+
 import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 /**
  * Created by hqythu on 12/29/2015.
@@ -37,6 +46,48 @@ public class ExeSQLTab extends JPanel {
         setLayout(new BorderLayout());
         add(controlPanel, BorderLayout.NORTH);
         add(resultScroll, BorderLayout.CENTER);
+
+        sqlExeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String sqlStr = sqlTextField.getText();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ParseResult parseResult = SQLParser.parse(sqlStr);
+                        java.util.List<Object[]> resultList = new ArrayList<>();
+                        try {
+                            resultList = QueryEngine.getInstance().query(
+                                    parseResult.selectOption, parseResult.where);
+                        } catch (Exception e) {
+                        }
+                        final java.util.List<Object[]> result = resultList;
+                        TableModel model = new AbstractTableModel() {
+                            @Override
+                            public int getRowCount() {
+                                return result.size();
+                            }
+
+                            @Override
+                            public int getColumnCount() {
+                                return result.get(0).length;
+                            }
+
+                            @Override
+                            public Object getValueAt(int rowIndex, int columnIndex) {
+                                return result.get(rowIndex)[columnIndex];
+                            }
+                        };
+                        SwingUtilities.invokeLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                executeResultTable.setModel(model);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
     }
 
 }
