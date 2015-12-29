a740e61ad0ed3c358008fcf9abe29eb3aec887d2
何钦尧 <hqythu@gmail.com>
2015-12-29 14:57:35 +0800

    ui: data tab

--- src/me/hqythu/wxydb/ui/DataTab.java
+++ src/me/hqythu/wxydb/ui/DataTab.java
@@ -1,11 +1,16 @@
 package me.hqythu.wxydb.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by hqythu on 12/29/2015.
  */
 public class DataTab extends JPanel {
+
+    private JPanel controlPanel;
+    private JComboBox tableSelectComboBox;
+    private DefaultComboBoxModel tableSelectModel;
 
     public DataTab() {
         SwingUtilities.invokeLater(new Runnable() {
@@ -17,6 +22,17 @@ public class DataTab extends JPanel {
     }
 
     private void createUi() {
+        setLayout(new BorderLayout());
+
+        controlPanel = new JPanel();
+        tableSelectModel = new DefaultComboBoxModel();
+        tableSelectComboBox = new JComboBox(tableSelectModel);
+        controlPanel.add(tableSelectComboBox);
+
+        add(controlPanel, BorderLayout.NORTH);
+    }
+
+    public void refreshDatabase() {
 
     }
 
